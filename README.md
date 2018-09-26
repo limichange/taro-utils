06b949780f1ed018fb5486740ae7db897b51c4b6
limichange <limichange@hotmail.com>
2018-09-26 11:51:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# taro-utils
+
+## Install
+
+```
+npm i taro-utils --save
+```
+
+## Usage
+
+```js
+import { route } from 'taro-utils'
+
+route.to('/pages/userCenter/index')
+```
---
+++ src/route.ts
@@ -0,0 +1,37 @@
+import Taro from '@tarojs/taro'
+
+export function append (url, params): string {
+  Object.keys(params).forEach((key, index) => {
+    const type = index === 0 ? '?' : '&'
+
+    url = `${url}${type}${key}=${params[key]}`
+  })
+
+  return url
+}
+
+export default {
+  redirect (url: string, params: Object = {}) {
+    Taro.redirectTo({
+      url: append(url, params)
+    })
+  },
+  to (url, params = {}) {
+    Taro.navigateTo({
+      url: append(url, params)
+    })
+  },
+  switch (url) {
+    Taro.switchTab({
+      url
+    })
+  },
+  back () {
+    Taro.navigateBack()
+  },
+  reLaunch (url) {
+    Taro.reLaunch({
+      url
+    })
+  }
+}
